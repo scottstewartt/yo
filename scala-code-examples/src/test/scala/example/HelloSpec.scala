406292394e207ae7517e44fed561e00ba22652ac
scottstewartt <scottstewartt@users.noreply.github.com>
2025-07-06 16:02:25 -0400

    Update README with sbt usage

---
+++ scala-code-examples/src/test/scala/example/HelloSpec.scala
@@ -0,0 +1,11 @@
+package example
+
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class HelloSpec extends AnyFlatSpec with Matchers {
+  "Hello" should "print greeting" in {
+    Hello.main(Array.empty)
+    succeed
+  }
+}
